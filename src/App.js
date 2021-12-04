3f55719a7fb7263b9e49fd78356cba6bcbe4f979
sarah <swhibley12@gmail.com>
2021-12-04 16:19:07 -0500

    set up a dictionary component, keyword state,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <small>Coded by Sarah Whibley</small>
+        </footer>
+      </div>
     </div>
   );
 }
